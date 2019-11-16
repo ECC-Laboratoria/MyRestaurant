59459a049a93728150acb473a2dbdbf283c8f1f6
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-11-16 12:38:10 -0600

    itunes example

--- Week8/Examples/iTunes_example.playground/Contents.swift
+++ Week8/Examples/iTunes_example.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+let baseURL = URL(string: "https://itunes.apple.com/search")!
+
+extension URL {
+    func withQueries(_ queries: [String:String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map{
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+        return components?.url
+    }
+}
+
+let query: [String:String] = [
+    "term":"movie",
+    "country":"CA",
+    "limit":"20"
+]
+
+let url = baseURL.withQueries(query)!
+
+let _ = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if let data = data, let iTunesResult = String(data: data, encoding: .utf8) {
+        print(iTunesResult)
+    }
+}.resume()
